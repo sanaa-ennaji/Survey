1989bb344ec86080a790921fbb579f4ccd7487b3
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-11 17:16:32 +0100

    interface service answer

--- src/main/java/com/sanaa/brif7/SurveyLens/service/interfaces/AnswerServiceI.java
+++ src/main/java/com/sanaa/brif7/SurveyLens/service/interfaces/AnswerServiceI.java
@@ -1,5 +1,15 @@
 package com.sanaa.brif7.SurveyLens.service.interfaces;
 
+import com.sanaa.brif7.SurveyLens.dto.request.AnswerCreateDTO;
+import com.sanaa.brif7.SurveyLens.dto.request.AnswerUpdateDTO;
+import com.sanaa.brif7.SurveyLens.dto.response.AnswerResponseDTO;
+import java.util.List;
+
 public interface AnswerServiceI {
 
+    AnswerResponseDTO create(AnswerCreateDTO createDTO);
+    AnswerResponseDTO findById(Long id);
+    List<AnswerResponseDTO> findAll();
+    void deleteById(Long id);
+    AnswerResponseDTO update(Long id, AnswerUpdateDTO updateDTO);
 }
