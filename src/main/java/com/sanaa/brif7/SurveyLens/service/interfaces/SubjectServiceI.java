3e710cdd647760f5518bc05d74007a2c0dd31a3e
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-11 17:18:22 +0100

    interface service subject

--- src/main/java/com/sanaa/brif7/SurveyLens/service/interfaces/SubjectServiceI.java
+++ src/main/java/com/sanaa/brif7/SurveyLens/service/interfaces/SubjectServiceI.java
@@ -1,4 +1,14 @@
 package com.sanaa.brif7.SurveyLens.service.interfaces;
+import com.sanaa.brif7.SurveyLens.dto.request.SubjectUpdateDTO;
+import com.sanaa.brif7.SurveyLens.dto.request.SubjectCreateDTO;
+import com.sanaa.brif7.SurveyLens.dto.response.SubjectResponseDTO;
+
+import java.util.List;
 
 public interface SubjectServiceI {
+    SubjectResponseDTO create(SubjectCreateDTO createDTO);
+    SubjectResponseDTO findById(Long id);
+    List<SubjectResponseDTO> findAll();
+    void deleteById(Long id);
+    SubjectResponseDTO update(Long id, SubjectUpdateDTO updateDTO);
 }
