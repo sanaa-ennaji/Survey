1d4e71c43c83cf5b8750ac9d5a63d374696f2c51
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-11 16:36:42 +0100

    repository question

--- src/main/java/com/sanaa/brif7/SurveyLens/repository/QuestionRepository.java
+++ src/main/java/com/sanaa/brif7/SurveyLens/repository/QuestionRepository.java
@@ -1,4 +1,7 @@
 package com.sanaa.brif7.SurveyLens.repository;
 
-public interface QuestionRepository {
+import com.sanaa.brif7.SurveyLens.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
